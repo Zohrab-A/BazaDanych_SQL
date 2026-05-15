00e07328877bff95bd0081168a06b686008effb0
Zohrab-A <zoro984@outlook.com>
2026-05-15 15:58:38 +0200

    dodanie

---
+++ BazaDanych/BazaDanych/SalesLT/Tables/VendorPriceHistory.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [SalesLT].[VendorPriceHistory] (
+    [QuoteID]   BIGINT   NULL,
+    [VendorID]  INT      NOT NULL,
+    [ProductID] INT      NOT NULL,
+    [Price]     MONEY    NOT NULL,
+    [QuoteDate] DATETIME NOT NULL
+);
+
+
+GO
+CREATE CLUSTERED INDEX [index_dwa]
+    ON [SalesLT].[VendorPriceHistory]([VendorID] ASC, [ProductID] ASC, [QuoteDate] ASC) WITH (FILLFACTOR = 25);
+
---
+++ BazaDanych/BazaDanych/dbo/Tables/TABELA_ZADANIE_5.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [dbo].[TABELA_ZADANIE_5] (
+    [ID]         INT NOT NULL,
+    [CustomerID] INT NULL,
+    [ProductID]  INT NULL,
+    [Ilosc]      INT NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC),
+    FOREIGN KEY ([CustomerID]) REFERENCES [240140].[Customer] ([CustomerID]),
+    FOREIGN KEY ([ProductID]) REFERENCES [SalesLT].[Product] ([ProductID])
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [index_jeden_zad5]
+    ON [dbo].[TABELA_ZADANIE_5]([CustomerID] ASC)
+    INCLUDE([ProductID], [Ilosc]);
+
+
+GO
+CREATE NONCLUSTERED INDEX [index_dwa_zad5]
+    ON [dbo].[TABELA_ZADANIE_5]([ProductID] ASC) WHERE ([Ilosc]>(0));
+
